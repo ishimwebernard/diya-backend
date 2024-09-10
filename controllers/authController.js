eb102cac0bc2cc45d5e4a69fb22e84fd7acac1ba
ishimwebernard <isbernard2001@gmail.com>
2024-09-10 15:11:43 +0200

    mend

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,40 +1,4 @@
 const Users = require( '../models').Users
-const signUp = async(req, res, next) => {
-    const body = req.body;
-    const findPhoneNumber = await Users.findOne({where: {phoneNumber: body.phoneNumber}})
-
-    if(findPhoneNumber){
-        return res.status(400).json({
-            message: 'Phone Number already in use'
-        })
-    }
-
-    if(!['1','2'].includes(body.userType) || !body.firstName || !body.lastName) {
-        return res.status(400).json({
-            status: 'Fail',
-            message: 'Enter all credentials'
-        })
-    }
-    const newUser = await Users.create({
-        userType: body.userType,
-        firstName: body.firstName,
-        lastName: body.lastName,
-        phoneNumber: body.phoneNumber,
-        password: body.password
-    })
-    
-    if (!newUser) {
-        return res.status(400).json({
-            status: 'Fail',
-            message: 'Failed to create the user'
-        })
-    }
-
-    return res.status(201).json({
-        status:'success',
-        message: 'user created'
-    })
-}
 
 const login = async(req, res, next) =>{
     const {phoneNumber, password} = req.body;
@@ -57,4 +21,67 @@ const login = async(req, res, next) =>{
     })
 }
 
-module.exports = {signUp, login}
+const addChildren = async(req, res, next) => {
+
+    
+}
+
+const signUp = async(req, res, next) => {
+    const body = req.body;
+    const findPhoneNumber = await Users.findOne({where: {phoneNumber: body.phoneNumber}})
+
+    if(findPhoneNumber){
+        return res.status(400).json({
+            message: 'Phone Number already in use'
+        })
+    }
+
+    if(!['1','2'].includes(body.userType) || !body.firstName || !body.lastName) {
+        return res.status(400).json({
+            status: 'Fail',
+            message: 'Enter all credentials'
+        })
+    }
+
+
+    const result = await Users.findOne({where: {phoneNumber: body.principal}});
+    console.log(result)
+
+    let existingChildren;
+    if (result !== null){
+        existingChildren = result.children == null ? "0":result.children
+        result.update({children:existingChildren+','+req.body.childPhoneNumber})
+    }else{
+        return res.status(400).json({message: 'Reference does not exist'})
+    }
+
+
+    const newUser = await Users.create({
+        userType: body.userType,
+        firstName: body.firstName,
+        lastName: body.lastName,
+        phoneNumber: body.phoneNumber,
+        password: body.password,
+        principal: body.principal
+    })
+    
+    if (!newUser) {
+        return res.status(400).json({
+            status: 'Fail',
+            message: 'Failed to create the user'
+        })
+    }
+
+    return res.status(201).json({
+        status:'success',
+        message: 'user created'
+    })
+}
+
+
+const getUserByPhone = async(req, res, next) => {
+    const userWithId = await Users.findOne({where: {phoneNumber: req.body.phoneNumber}})
+
+    return res.status(200).json({userWithId})
+}
+module.exports = {signUp, login, addChildren, getUserByPhone}
--- routes/authroute.js
+++ routes/authroute.js
@@ -1,7 +1,8 @@
-const {signUp, login}  =  require('../controllers/authController')
+const {signUp, login, addChildren, getUserByPhone}  =  require('../controllers/authController')
 const router = require('express').Router();
 
 router.route('/signup').post(signUp)
 router.route('/login').post(login)
-
+router.route('/addchildren').post(addChildren)
+router.route('/getUserByPhone').post(getUserByPhone)
 module.exports = router
